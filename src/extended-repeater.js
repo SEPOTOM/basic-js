66f9df701b21062897629ebd7435424bc714bc22
Artyom Pizin <artempetr2016@gmail.com>
2023-04-13 18:37:01 +0300

    feat: implement repeater function

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater( str, options ) {
+
+  const addition = typeof options.addition === 'undefined' ? '' : `${ options.addition }`;
+
+  const additionArray = new Array( options.additionRepeatTimes || 1 );
+  additionArray.fill( addition );
+  const stringAddition = additionArray.join( options.additionSeparator || '|' );
+
+  const strArray = new Array( options.repeatTimes || 1 );
+  strArray.fill( `${ str }${ stringAddition }` );
+
+  return strArray.join( options.separator || '+' );
+
 }
 
 module.exports = {
