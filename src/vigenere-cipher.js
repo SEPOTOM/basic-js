0ab2769b8f813523f747019276a3ddca05f58f4c
Artyom Pizin <artempetr2016@gmail.com>
2023-04-16 17:25:22 +0300

    feat: implement VigenereCipheringMachine class

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,14 +20,124 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor( direct ) {
+
+    this._direct = direct === false ? false : true;
+    this._range = [ 0, 25 ];
+    this._totalChars = 26;
+    this._minimumCharCode = 65;
+
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt( message, keyword ) {
+
+    if ( !message || !keyword ) {
+
+      throw new Error( 'Incorrect arguments!' );
+
+    }
+
+    let encryptedMessage = '';
+    let messageCharUpperCase = null;
+    let keywordCharUpperCase = null;
+
+    for ( let i = 0, k = 0; i < message.length; i++, k++ ) {
+
+      if ( k === keyword.length ) {
+
+        k = 0;
+
+      }
+
+      messageCharUpperCase = message[ i ].toUpperCase();
+      keywordCharUpperCase = keyword[ k ].toUpperCase();
+
+      if ( messageCharUpperCase.charCodeAt( 0 ) === 32 ) {
+
+        k--;
+
+      }
+
+      if ( messageCharUpperCase.charCodeAt( 0 ) - this._minimumCharCode < this._range[ 0 ] || messageCharUpperCase.charCodeAt( 0 ) - this._minimumCharCode > this._range[ 1 ] ) {
+
+        encryptedMessage += messageCharUpperCase;
+        continue;
+
+      }
+
+      const codesSum = ( messageCharUpperCase.charCodeAt( 0 ) - this._minimumCharCode ) + ( keywordCharUpperCase.charCodeAt( 0 ) - this._minimumCharCode );
+
+      if ( codesSum > this._range[ 1 ] ) {
+
+        encryptedMessage += String.fromCharCode( codesSum - this._totalChars + this._minimumCharCode );
+
+      } else {
+
+        encryptedMessage += String.fromCharCode( codesSum + this._minimumCharCode );
+
+      }
+
+    }
+
+    return this._direct ? encryptedMessage : encryptedMessage.split( '' ).reverse().join( '' );
+
   }
+
+  decrypt( message, keyword ) {
+
+    if ( !message || !keyword ) {
+
+      throw new Error( 'Incorrect arguments!' );
+
+    }
+
+    let decryptedMessage = '';
+    let messageChar = null;
+    let keywordCharUpperCase = null;
+
+    for ( let i = 0, k = 0; i < message.length; i++, k++ ) {
+
+      if ( k === keyword.length ) {
+
+        k = 0;
+
+      }
+
+      messageChar = message[ i ];
+      keywordCharUpperCase = keyword[ k ].toUpperCase();
+
+      if ( messageChar.charCodeAt( 0 ) === 32 ) {
+
+        k--;
+
+      }
+
+      if ( messageChar.charCodeAt( 0 ) - this._minimumCharCode < this._range[ 0 ] || messageChar.charCodeAt( 0 ) - this._minimumCharCode > this._range[ 1 ] ) {
+
+        decryptedMessage += messageChar;
+        continue;
+
+      }
+
+      const codesDiff = ( messageChar.charCodeAt( 0 ) - this._minimumCharCode ) - ( keywordCharUpperCase.charCodeAt( 0 ) - this._minimumCharCode );
+
+      if ( codesDiff < this._range[ 0 ] ) {
+
+        decryptedMessage += String.fromCharCode( codesDiff + this._totalChars + this._minimumCharCode );
+
+      } else {
+
+        decryptedMessage += String.fromCharCode( codesDiff + this._minimumCharCode );
+
+      }
+
+    }
+
+    return this._direct ? decryptedMessage : decryptedMessage.split( '' ).reverse().join( '' );
+
+  }
+
 }
 
 module.exports = {
